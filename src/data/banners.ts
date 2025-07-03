c22252aa6840866ad41851af6017a27a3c97e9cf
Mia Su <miasu713@gmail.com>
2025-07-03 15:58:56 +0800

    圖片路徑調整

--- src/data/banners.ts
+++ src/data/banners.ts
@@ -1,33 +1,33 @@
 export const bannerImages = [
   { 
-    src: "/pc_促銷Banner.jpg", 
+    src: "./pc_促銷Banner.jpg", 
     alt: "Sale Banner", 
     "data-ai-hint": "product sale",
-    srcMobile: "/md_促銷Banner.jpg",
+    srcMobile: "./md_促銷Banner.jpg",
     "data-ai-hint-mobile": "product sale",
     href: "/products/20"
   },
   { 
-    src: "/1200x800-1.jpg", 
+    src: "./1200x800-1.jpg", 
     alt: "About Me Banner", 
     "data-ai-hint": "cute girl",
-    srcMobile: "/640x800-1.jpg",
+    srcMobile: "./640x800-1.jpg",
     "data-ai-hint-mobile": "cute girl",
     href: "https://mia-sheep-su.github.io/AboutMe/"
   },
   { 
-    src: "/1200x800-2.jpg", 
+    src: "./1200x800-2.jpg", 
     alt: "Hamster Banner", 
     "data-ai-hint": "Hamster",
-    srcMobile: "/640x800-2.jpg",
+    srcMobile: "./640x800-2.jpg",
     "data-ai-hint-mobile": "Hamster",
     href: "https://mia-sheep-su.github.io/MyHamsterTest/"
   },
   { 
-    src: "/1200x800.jpg", 
+    src: "./1200x800.jpg", 
     alt: "example", 
     "data-ai-hint": "example",
-    srcMobile: "/640x800.jpg",
+    srcMobile: "./640x800.jpg",
     "data-ai-hint-mobile": "example",
     href: "/products/2"
   },
