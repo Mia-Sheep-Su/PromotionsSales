9b7cc8b69e131e7de1664f6968a4835eeaffdabd
Mia Su <miasu713@gmail.com>
2025-07-03 17:13:46 +0800

    到計時器修改

--- src/data/countdown.ts
+++ src/data/countdown.ts
@@ -1,3 +1,6 @@
-// To configure the countdown, change the date and time string below.
-// The format is "YYYY-MM-DDTHH:mm:ss".
-export const countdownTargetDate = new Date('2025-7-13T23:59:59');
+// To configure the countdown, change the numbers below.
+// The format is: new Date(year, monthIndex, day, hours, minutes, seconds)
+// IMPORTANT: The month is 0-based, so 0 is January, 6 is July, and 11 is December.
+
+// This date corresponds to July 13, 2025 at 23:59:59
+export const countdownTargetDate = new Date(2025, 7, 13, 23, 59, 59);
